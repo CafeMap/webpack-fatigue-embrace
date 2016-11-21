2c9cc18306935297dcbd93681159a915e952440a
Whien <sal95610@gmail.com>
2016-11-21 14:01:17 +0800

    changed test

--- test/webpack.build.config.js
+++ test/webpack.build.config.js
@@ -1,11 +1,26 @@
 let WebapckFatigueEmbrace = require('../index')
 
 let options = {
+  libOnly: true,
+  output: {
+    library: 'WebpackFatigueEmbrace',
+    libraryTarget: 'umd'
+  },
+  externals: [
+    'react'
+  ],
   plugins: {
-    optimize: ['OccurrenceOrder']
+    normal: [
+      ['Define', { 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }] // DefinePlugin
+    ],
+    optimize: [
+      ['OccurrenceOrder'], // OccurrenceOrderPlugin
+      ['UglifyJs', { compress: { warnings: false } }] // UglifyJs options
+    ]
   }
 }
 
 let wfe = new WebapckFatigueEmbrace(options)
+console.log(wfe)
 
 module.exports = wfe
